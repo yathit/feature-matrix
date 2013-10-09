00d5fad642b192d43e5763d8102221bb2b686e96
kyaw <kyaw@blueprint.org>
2013-10-09 10:45:19 +0800

    report versioning

--- app/js/app.js
+++ app/js/app.js
@@ -8,3 +8,11 @@ angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 
     $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'HomeCtrl'});
     $routeProvider.otherwise({redirectTo: '/home'});
   }]);
+
+
+var handleClientLoad = function() {
+  gapi.client.setApiKey('AIzaSyCJiBR1-tmt0Pp2yhBo8P7g6FqY2q_S7F8');
+  gapi.client.load('storage', 'v1beta2', function() {
+    console.log('gapi ready')
+  });
+};
