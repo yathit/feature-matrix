3557f4e4ee24f080cf1dfc447f424d9f68bebe4e
Kyaw Tun <kyawtun@yathit.com>
2014-09-11 18:27:00 +0800

    update

--- scripts/gcs/setup.sh
+++ scripts/gcs/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # setup test report data bucket
 
-TBK="ydn-test-report-2"
+TBK="ydn-test-report-3"
 
 gsutil mb gs://$TBK
 
-gsutil acl set acl.xml gs://$TBK
-gsutil defacl set default-acl.xml gs://$TBK
-gsutil cors set cors.xml  gs://$TBK
+gsutil acl set acl.json gs://$TBK
+gsutil defacl set defacl.json gs://$TBK
+gsutil cors set cors.json  gs://$TBK
